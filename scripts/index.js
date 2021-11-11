7c8958c18d04e79c13c4a4f6e7c1cc28eb5ff9c0
BuildTools <unconfigured@null.spigotmc.org>
2021-11-10 23:14:04 -0600

    making progress

--- scripts/index.js
+++ scripts/index.js
@@ -15,3 +15,23 @@ function getPost(){
         console.log(error);
     });
 }
+
+function addPost(){
+    const addPostsApi = "https://localhost:5001/api/post";
+    const postText = document.getElementById("text").value;
+    
+    fetch(addPostsApi, {
+        method: "POST",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json'
+        },
+        body : JSON.stringify({
+            text: postText
+        })
+    })
+    .then((response)=>{
+        console.log(response)
+        getPost();
+    })
+}
